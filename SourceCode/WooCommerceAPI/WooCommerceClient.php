b224df8d0d245d77d0d7f4f3096add4af3f9ff51
thanhthanh54 <thanhuitk13@gmail.com>
2022-09-28 22:04:29 +0700

    woocommerce-api-code

---
+++ SourceCode/WooCommerceAPI/WooCommerceClient.php
@@ -0,0 +1,19 @@
+<?php
+// Install:
+// composer require automattic/woocommerce
+
+// Setup:
+require __DIR__ . '/vendor/autoload.php';
+
+use Automattic\WooCommerce\Client;
+
+$woocommerce = new Client(
+    'http://example.com', // Your store URL
+    'consumer_key', // Your consumer key
+    'consumer_secret', // Your consumer secret
+    [
+        'wp_api' => true, // Enable the WP REST API integration
+        'version' => 'wc/v3' // WooCommerce WP REST API version
+    ]
+);
+?>
